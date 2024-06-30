66122360025aedcc232c0d555f704c4a680a9085
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-07-01 01:47:21 +0200

    update server

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,26 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World from vercel update!';
+  users = [
+    {
+      id: 1,
+      name: 'user1',
+      age: 25
+    },
+    {
+      id: 2,
+      name: 'user2',
+      age: 35
+    },
+    {
+      id: 3,
+      name: 'user3',
+      age: 55
+    },
+  
+    ]
+    
+    getUsers() {
+      return this.users;
+    }
   }
-}
